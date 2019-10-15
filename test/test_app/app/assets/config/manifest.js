f05dd0e87edd29aa760cfb6c6802afbc62b6ba8c
BradPotts <brad.potts@phcnet.co>
2019-10-15 11:47:26 +0000

    updated test_app fixes to layout.

--- test/test_app/app/assets/config/manifest.js
+++ test/test_app/app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link phcdevworks_press_manifest.js
+//= link phcdevworks_accounts/application.css
+//= link phcdevworks_accounts/application.js
+//= link phcdevworks_press/application.css
+//= link phcdevworks_press/application.js
