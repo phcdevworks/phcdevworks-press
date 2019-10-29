e8e5db9d8cbb5ad5eac9f721001606b86d050ad1
BradPotts <brad.potts@phcnet.co>
2019-10-29 23:35:44 +0000

    updated phcaccountspro.

--- app/models/phcdevworks_press/article/post.rb
+++ app/models/phcdevworks_press/article/post.rb
@@ -25,7 +25,7 @@ module PhcdevworksPress
     friendly_id :phcdev_article_post_nice_urls, use: [:slugged, :finders]
 
     def phcdev_article_post_nice_urls
-      [:post_title]
+      [:project_post_title]
     end
 
   end
