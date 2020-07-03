49cf762392af180027965fcee90f968fe18aa0f5
BradPotts <brad.potts@phcnet.co>
2020-07-03 11:12:36 +0000

    adjusted remove cols migration.

--- test/test_app/db/migrate/20200703111123_remove_images_cols_from_phcdevworks_press.phcdevworks_press.rb
+++ test/test_app/db/migrate/20200703111123_remove_images_cols_from_phcdevworks_press.phcdevworks_press.rb
@@ -1,5 +1,5 @@
 # This migration comes from phcdevworks_press (originally 20200702121306)
-class RemoveImagesColsFromPhcdevworksPressArticlePosts < ActiveRecord::Migration[6.0]
+class RemoveImagesColsFromPhcdevworksPress < ActiveRecord::Migration[6.0]
   def change
 
     remove_column :phcdevworks_press_article_posts, :post_image, :string
