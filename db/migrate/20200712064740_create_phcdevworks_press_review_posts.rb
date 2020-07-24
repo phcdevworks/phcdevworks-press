dc37a9685598564ba6aae418a63d1f29e764a6d1
BradPotts <brad.potts@phcnet.co>
2020-07-24 11:23:00 +0000

    updated db ording won't affect current install.

--- db/migrate/20200712064740_create_phcdevworks_press_review_posts.rb
+++ db/migrate/20200712064740_create_phcdevworks_press_review_posts.rb
@@ -4,8 +4,9 @@ class CreatePhcdevworksPressReviewPosts < ActiveRecord::Migration[6.0]
 
       t.string :review_post_title
       t.text :review_post_text
-      t.string :review_post_status
       t.integer :review_post_rating
+      t.string :review_post_status
+
 
       t.string :optimization_id
 
--- db/migrate/20200712064920_create_phcdevworks_press_list_items.rb
+++ db/migrate/20200712064920_create_phcdevworks_press_list_items.rb
@@ -2,10 +2,10 @@ class CreatePhcdevworksPressListItems < ActiveRecord::Migration[6.0]
   def change
     create_table :phcdevworks_press_list_items do |t|
 
+      t.integer :list_item_number
       t.string :list_item_title
       t.text :list_item_text
       t.string :list_item_url
-      t.integer :list_item_number
 
       t.string :optimization_id
 
