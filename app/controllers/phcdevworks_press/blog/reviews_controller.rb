429f36816047ac309763cd950fa030282cc1ad72
BradPotts <brad.potts@phcnet.co>
2020-09-02 23:38:45 +0000

    add blog all to blog controllers.

--- app/controllers/phcdevworks_press/blog/reviews_controller.rb
+++ app/controllers/phcdevworks_press/blog/reviews_controller.rb
@@ -1,20 +1,29 @@
 require_dependency "phcdevworks_press/application_controller"
 
 module PhcdevworksPress
-  class Blog::ReviewsController < ApplicationController
+    class Blog::ReviewsController < ApplicationController
 
-    # Layout
-    layout "phcdevworks_press/frontend"
+        # Filters
+        before_action :phc_blog_index_all
 
-    # INDEX
-    def index
-      @phcdevworks_cms_press_review_index = Review::Post.where(review_post_status: "published").order("created_at DESC")
+        # Layout
+        layout "phcdevworks_press/frontend"
+
+        # INDEX
+        def index
+        end
+
+        # SHOW
+        def show
+            @phcdevworks_cms_press_review_single = Review::Post.friendly.find(params[:id])
+        end
+
+        # Index All for Blog
+        def phc_blog_index_all
+            @phcdevworks_cms_press_article_index = Article::Post.where(article_post_status: "published").order("created_at DESC")
+            @phcdevworks_cms_press_list_index = List::Post.where(list_post_status: "published").order("created_at DESC")
+            @phcdevworks_cms_press_review_index = Review::Post.where(review_post_status: "published").order("created_at DESC")
+        end
+
     end
-
-    # SHOW
-    def show
-      @phcdevworks_cms_press_review_single = Review::Post.friendly.find(params[:id])
-    end
-
-  end
 end
